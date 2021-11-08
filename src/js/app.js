64303754ecbca280c70930d2272efcf486d6e3ac
Gronik64 <golegni@yandex.ru>
2021-11-08 21:19:54 +0300

    syntax eslint

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,4 @@
 export default function sortValue(ob, order) {
-  let res = [];
-  for (const item of order) {
-    res.push({ key: item, value: ob[item] });
-    delete ob[item];
-  }
   const res1 = [];
   for (const item in ob) {
     res1.push({ key: item, value: ob[item] });
@@ -12,6 +7,11 @@ export default function sortValue(ob, order) {
     const j = (a.key > b.key) ? 1 : -1;
     return j;
   });
+  let res = [];
+  for (const item of order) {
+    res.push({ key: item, value: ob[item] });
+    res1.splice(res1.indexOf(item), 1);
+  }
   res = res.concat(res1);
   return res;
 }
